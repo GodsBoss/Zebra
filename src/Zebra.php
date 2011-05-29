b04830b82d68a03d6608a774c7e02684a2178cd7
GodsBoss <godsboss@gmx.de>
2011-05-29 22:26:55 +0200

    Add default striping.

--- src/Zebra.php
+++ src/Zebra.php
@@ -1,3 +1,12 @@
 <?php
 
-class Zebra{}
+class Zebra{
+	private $state = 'even';
+
+	private static $states = array(
+		'odd' => 'even',
+		'even' => 'odd');
+
+	public function __toString(){
+		$this->state = self::$states[$this->state];
+		return $this->state;}}
--- test/Zebra/ZebraTest.php
+++ test/Zebra/ZebraTest.php
@@ -2,4 +2,8 @@
 
 class ZebraTest extends PHPUnit_Framework_TestCase{
 	public function test_instantiation(){
-		$zebra = new Zebra();}}
+		$zebra = new Zebra();}
+
+	public function test_default_striping(){
+		$zebra = new Zebra();
+		$this->assertEquals('odd even odd even odd', $zebra.' '.$zebra.' '.$zebra.' '.$zebra.' '.$zebra);}}
