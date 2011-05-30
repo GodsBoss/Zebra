5f9b7605512803063ec9f52341440b7a78d81d43
GodsBoss <godsboss@gmx.de>
2011-05-30 22:34:08 +0200

    Extracted creation of states and state sets into separate methods.

--- src/Zebra.php
+++ src/Zebra.php
@@ -5,16 +5,26 @@ class Zebra{
 
 	private $stateSets = array();
 
+	private function createStates($sets){
+		foreach($sets as $set){
+			$this->states[] = $set[count($set)-1];}}
+
+	private function createStateSet($set){
+		$stateSet = array();
+		for($i=0, $l=count($set); $i<$l; $i++){
+			$stateSet[$set[$i]] = $set[($i+1)%$l];}
+		$this->stateSets[] = $stateSet;}
+
+	private function createStateSets($sets){
+		foreach($sets as $set){
+			$this->createStateSet($set);}}
+
 	public function __construct(array $states = array('odd', 'even')){
 		$sets = func_get_args();
 		if (count($sets)==0){
 			$sets = array($states);}
-		foreach($sets as $set){
-			$stateSet = array();
-			for($i=0, $l=count($set); $i<$l; $i++){
-				$stateSet[$set[$i]] = $set[($i+1)%$l];}
-			$this->stateSets[] = $stateSet;
-			$this->states[] = $set[count($set)-1];}}
+		$this->createStateSets($sets);
+		$this->createStates($sets);}
 
 	private function next(){
 		foreach($this->states as $index => $value){
